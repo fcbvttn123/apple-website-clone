6a9ee9b8ee5365a90403f50bb47700d4f3df572f
ngan <vutran@sheridancollege.ca>
2025-01-05 17:09:04 -0500

    Implement animation for the title of hero section using GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
 export function Hero() {
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 })
+  }, [])
   return (
     <section className="w-full relative bg-black h-[calc(100vh-60px)]">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
       </div>
     </section>
   )
