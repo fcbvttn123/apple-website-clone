e42c1d54120af5e0c0d193348ea2b9fb64a8f634
ngan <vutran@sheridancollege.ca>
2025-01-13 19:52:54 -0500

    Update path for glb file for Iphone component and include Iphone and Lights components to ModelView component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,32 @@
-export function ModelView() {
+import { PerspectiveCamera, View } from "@react-three/drei"
+import { Lights } from "./Lights"
+import { Iphone } from "./Iphone"
+import { Suspense } from "react"
+
+export function ModelView({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) {
   return (
-    <div>
-      <h1>Model View</h1>
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute border-2 border-red-500 ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+    >
+      {/* <ambientLight intensity={0.3} /> */}
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <Suspense>
+        <Iphone />
+      </Suspense>
+    </View>
   )
 }
